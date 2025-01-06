56b25508d5794a1b7ead966d166188c4fd46eb43
saul.imbrici@sonicitconsulting.it <saul.imbrici@sonicitconsulting.it>
2025-01-07 00:44:56 +0100

    find broken links

--- pyseoanalyzer/page.py
+++ pyseoanalyzer/page.py
@@ -654,7 +654,8 @@ class Page:
                 # Effettua una richiesta HEAD per controllare il link
                 response = requests.head(absolute_url, allow_redirects=True, timeout=5)
                 if response.status_code != 200:
-                    self.warn(f"Url {self.url} is broken - Status code: {str(response.status_code)}")
+                    code = str(response.status_code)
+                    self.warn(f"Url {self.url} is broken - Status code: {code}")
             except requests.RequestException as e:
                 # Se c'è un errore, aggiungi il link alla lista di link non raggiungibili
                 self.warn(f"Url {self.url} is broken - Error {e}")
